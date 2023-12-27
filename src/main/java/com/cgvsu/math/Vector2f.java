433fab7876fe6680cf10dd4283a47cad06cac6fb
wqeky <wqeky@mail.ru>
2023-12-27 15:55:04 +0300

    в векторах поправка

--- src/main/java/com/cgvsu/math/Vector2f.java
+++ src/main/java/com/cgvsu/math/Vector2f.java
@@ -1,6 +1,4 @@
 package com.cgvsu.math;
-
-
 public class Vector2f {
     public Vector2f(float x, float y) {
         this.x = x;
--- src/main/java/com/cgvsu/math/Vector3f.java
+++ src/main/java/com/cgvsu/math/Vector3f.java
@@ -4,8 +4,6 @@ public class Vector3f {
     public float x;
     public float y;
     public float z;
-
-
     public Vector3f(float x, float y, float z) {
         this.x = x;
         this.y = y;
@@ -52,7 +50,6 @@ public class Vector3f {
     }
 
     public boolean equals(Vector3f other) {
-        // todo: желательно, чтобы это была глобальная константа
         final float eps = 1e-7f;
         return Math.abs(x - other.x) < eps && Math.abs(y - other.y) < eps && Math.abs(z - other.z) < eps;
     }
